a5263dfe0fa8615dead15fa48a1624a0bcdb93f6
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2019-03-02 14:18:28 +0100

    Unit tests for the Configuration in the IoC project

---
+++ UnitTests/ConfigurationTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using HelloIoCConfiguration;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ConfigurationTests
+    {
+        private readonly Configuration _sut;
+
+        public ConfigurationTests()
+        {
+            _sut = new Configuration(key => $"FAKE-{key}");
+        }
+
+        [Fact]
+        public void NumberOfFriends_Should_Return_Expected()
+        {
+            var result = _sut.NumberOfFriends;
+
+            result.Should().Be("FAKE-NumberOfFriends");
+        }
+
+        [Fact]
+        public void RandomPersonGeneratorApiLocation_Should_Return_Expected()
+        {
+            var result = _sut.RandomPersonGeneratorApiLocation;
+
+            result.Should().Be("FAKE-RandomPersonGeneratorApiLocation");
+        }
+    }
+}
